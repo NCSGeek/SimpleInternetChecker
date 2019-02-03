2c987dca23e2b6dfe4b9fddde3aab989476dfbab
NCSGeek <ncsjohn2001@gmail.com>
2019-02-03 00:27:54 -0500

    Initial Commit

---
+++ SimpleInternetConnectionCheck/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SimpleInternetConnectionCheck
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 6.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 10);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(Click to check)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Internet Status";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(125, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.toolTip.SetToolTip(this.pictureBox1, "Click to refresh internet status");
+            this.pictureBox1.Click += new System.EventHandler(this.buttonRefresh);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(304, 175);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Internet Checker";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SimpleInternetConnectionCheck/MainForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFirewallHelper;
+
+namespace SimpleInternetConnectionCheck
+{
+    public partial class MainForm : System.Windows.Forms.Form
+    {
+        bool allow = true;
+        private void refreshStatus()
+        {
+            if (allow == true)
+            {
+                Console.WriteLine("Checking internet connection...");
+                
+                allow = false;
+                try
+                {
+                    using (var client = new System.Net.WebClient())
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        label2.ForeColor = Color.Green;
+                        Console.WriteLine("Internet Connection Test Successful");
+                    }
+                }
+                catch
+                {
+                    label2.ForeColor = Color.Red;
+                    Console.WriteLine("Internet Connection Test Failed");
+                }
+                allow = true;
+            }
+        }
+
+        private static void createFirewallRule()
+        {
+            var ruleOut = FirewallManager.Instance.CreateApplicationRule(
+        FirewallManager.Instance.GetProfile().Type, "InternetChecker",
+        FirewallAction.Allow, Application.ExecutablePath);
+            ruleOut.Direction = FirewallDirection.Outbound;
+
+            var ruleIn = FirewallManager.Instance.CreateApplicationRule(
+        FirewallManager.Instance.GetProfile().Type, "InternetChecker",
+        FirewallAction.Allow, Application.ExecutablePath);
+            ruleIn.Direction = FirewallDirection.Inbound;
+
+            FirewallManager.Instance.Rules.Add(ruleOut);
+            FirewallManager.Instance.Rules.Add(ruleIn);
+        }
+
+        private void buttonRefresh(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(refreshStatus)
+            {
+                IsBackground = true
+            };
+            thread.Start();
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            var allRules = FirewallManager.Instance.Rules.ToArray();
+            bool exists = false;
+            foreach (var s in allRules)
+            {
+                if (s.ToString() == "InternetChecker")
+                {
+                    exists = true;
+                }
+            }
+            if (exists == false)
+            {
+                createFirewallRule();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            //refreshStatus();
+        }
+    }
+}
